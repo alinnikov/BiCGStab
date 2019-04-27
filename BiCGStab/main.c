1fd9b9f714cc4c6a244c88dbf3ef417e52713088
linnikov1 <linnikov1@gmail.com>
2019-04-27 23:40:38 +0300

    Fixes

--- BiCGStab/main.c
+++ BiCGStab/main.c
@@ -20,7 +20,7 @@ int main(void)
 	double eps = 0.00000001;
 	int max_iterations = 3000000;
 
-	char name[] = "poli.rb";
+	char name[] = "ex35.rb";
 	read_csr_matrix(&m, name);
 
 	double *x_n = (double*)malloc((m.num_rows) * sizeof(double));
